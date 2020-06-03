06bfebac364003a968730ac6468cc7e29a715f2a
Sandro0330 <60124712+Sandro0330@users.noreply.github.com>
2020-06-03 02:25:56 -0300

    Classe e entidades Vendedor

--- src/aplication/Programa.java
+++ src/aplication/Programa.java
@@ -1,12 +1,17 @@
 package aplication;
 
+import java.util.Date;
+
 import model.entities.Departamento;
+import model.entities.Vendedor;
 
 public class Programa {
 	
 	public static void main(String[] args) {
 		
 		Departamento obj = new Departamento(1, "Books");
+		Vendedor vendedor = new Vendedor(21, "Regina", "regina@gmail.com", new Date(), 3000.0, obj);
+		System.out.println(vendedor);
 		System.out.println(obj);
 	}
 
